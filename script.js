967d08e42243246f764aea9007a64f04a26bc463
Igor Matheus <mattigor.impr@gmail.com>
2023-02-28 11:45:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const buttonOpenModal = document.querySelector("button");
-const modal = document.querySelector("dialog");
-const buttonCloseModal = document.querySelector("dialog button");
+function calculaIMC(altura, massa, resultado, valorAltura, valorMassa, imc) {
+    altura = document.querySelector("input#article-imc-input-height");
+    massa = document.querySelector("input#article-imc-input-weight");
+    resultado = document.querySelector("span#resultado");
+    valorAltura = Number(altura.value);
+    valorMassa = Number(massa.value);
+    imc = valorMassa / (valorAltura * valorAltura);
 
-buttonOpenModal.onclick = function() {
-    modal.showModal();
-}
-
-buttonCloseModal.onclick = function() {
-    modal.close();
-}
+    if (imc < 18) {
+        resultado.innerHTML = `Seu IMC é ${imc.toFixed(1)} e, segundo a OMS, você está abaixo do peso.`;
+    } else if ((imc > 18) && (imc < 25)) {
+        resultado.innerHTML = `Seu IMC é ${imc.toFixed(1)} e, segundo a OMS, você está no seu peso normal.`;
+    } else if ((imc > 25) && (imc < 30)) {
+        resultado.innerHTML = `Seu IMC é ${imc.toFixed(1)} e, segundo a OMS, você está com excesso de peso.`;
+    } else if ((imc > 30) && (imc < 35)) {
+        resultado.innerHTML = `Seu IMC é ${imc.toFixed(1)} e, segundo a OMS, você está com obesidade classe I.`;
+    } else if ((imc > 35) && (imc < 40)) {
+        resultado.innerHTML = `Seu IMC é ${imc.toFixed(1)} e, segundo a OMS, você está com obesidade classe II.`;
+    } else {
+        resultado.innerHTML = `Seu IMC é ${imc.toFixed(1)} e, segundo a OMS, você está com obesidade classe III.`;
+    }
+};
